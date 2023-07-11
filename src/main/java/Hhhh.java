042931fa10d0c1699eb63c95864315622e862084
Hieununu <khachieu201202@gmail.com>
2023-07-11 17:35:19 +0700

    1

--- src/main/java/Hhhh.java
+++ src/main/java/Hhhh.java
@@ -1,6 +1,150 @@
+
+import java.util.Scanner;
 public class Hhhh {
-    public static void main(String[] args){
-        System.out.println("hello");
-    }
+//    public static void main(String[] args){
+//        System.out.println("hello");
+//    }
+
+
+
+//public static void main(String[] args){
+//    Scanner input = new Scanner(System.in);
+//    System.out.println("nhap 1 so bat ky tu 0 den 9:");
+//    int a = input.nextInt();
+//    switch(a){
+//        case 0: System.out.println("khong");break;
+//        case 1:System.out.println("mot");break;
+//        case 2:System.out.println("hai");break;
+//        case 3:System.out.println("ba");break;
+//        case 4:System.out.println("bon");break;
+//        case 5:System.out.println("nam");break;
+//        case 6:System.out.println("sau");break;
+//        case 7:System.out.println("bay");break;
+//        case 8:System.out.println("tam");break;
+//        default:
+//            System.out.println("chi co tu 0 den 9 thoi :))) ");
+//    }
+//}
+
+
+
+//    public static void main(String[] args)
+//    {
+//        float a, b, c;
+//        System.out.println("nhap 3 so bat ky:");
+//        Scanner sc = new Scanner(System.in);
+//
+//        a = sc.nextFloat();
+//        b = sc.nextFloat();
+//        c = sc.nextFloat();
+//
+//        if(a+b>c && b+c>a && c+a>b)
+//            System.out.println("la 3 canh cua 1 am giac");
+//        else
+//            System.out.println("khong la 3 canh cua 1 tam giac");
+//    }
+
+
+
+
+//    public static void main(String[] args){
+//
+//
+//            float a, b, c;
+//            System.out.println("nhap 3 so bat ky:");
+//            Scanner sc = new Scanner(System.in);
+//
+//            a = sc.nextFloat();
+//            b = sc.nextFloat();
+//            c = sc.nextFloat();
+//
+//            if(a*a+b*b==c*c || b*b+c*c==a*a || c*c+a*a==b*b)
+//                System.out.println("la 3 canh cua tam giac vuong");
+//            else
+//                System.out.println("khong phai 3 canh cua am giac vuong");
+//        }
+
+
+
+
+
+//    public static void main(String [] args){
+//        int tong = 0;
+//           Scanner sc = new Scanner(System.in);
+//           System.out.println("nhap n= ");
+//           int n = sc.nextInt();
+//           for(int i = 0;i <=n;i++){
+//               tong += i;
+//               System.out.println(tong);
+//           }
+//    }
+
+
+
+
+//    public static void main(String[] args){
+//        int n;
+//        System.out.println("nhap so nguyen:");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for (int i = 1; i <= 20; i++)
+//        {
+//            System.out.println(n + " x " + i + " = " + n*i);
+//        }
+//    }
+
+
+
+
+//public static void main(String[] args) {
+//    int a;
+//    a = binhPhuong();
+//    System.out.println("binh phuong cua 10 la:"+a);
+//}
+//public static int binhPhuong(){
+//    return 10*10;
+//}
+
+
+
+//    public static void main(String[] args) {
+//        int ketQua, n;
+//
+//        n = 3;
+//        ketQua = tinhBinhPhuong(n);
+//        System.out.println("binh phuong cua 3 la: " + ketQua);
+//
+//        n = 4;
+//        ketQua = tinhBinhPhuong(n);
+//        System.out.println("binh phuong cua 4 la: " + ketQua);
+//
+//        n = 5;
+//        ketQua = tinhBinhPhuong(n);
+//        System.out.println("binh phuong cua 5 la: " + ketQua);
+//    }
+//
+//    static int tinhBinhPhuong(int i) {
+//        return i * i;
+//    }
+
+
+
+
+
+//    public static int tong(int a,int b){
+//        return a + b;
+//    }
+//    public static int tich(int x,int y){
+//        return x * y;
+//    }
+//    public static void main(String[] args){
+//        System.out.println("tong cua 20 va 30 la:" + tong(20,30));
+//        System.out.println("tich cua 20 va 30 la:" + tich(20,30));
+//    }
+
 
 }
+
+
+
+
